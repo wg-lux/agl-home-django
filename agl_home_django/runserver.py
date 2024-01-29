09d91f1ddfaa3d3a6210ab30ce8df7946180cf59
maddonix <tlux14@googlemail.com>
2024-01-29 02:05:22 +0100

    fix paths

--- agl_home_django/runserver.py
+++ agl_home_django/runserver.py
@@ -4,6 +4,6 @@ import subprocess
 # Change path? /lib/python3.11/site-packages/agl_home_django
 
 def start_django():
-    subprocess.run(["python", "agl_home_django/manage.py", "makemigrations"])
-    subprocess.run(["python", "agl_home_django/manage.py", "migrate"])
-    subprocess.run(["python", "agl_home_django/manage.py", "runserver"])
+    subprocess.run(["python", "/home/agl-admin/agl-home-django/agl_home_django/manage.py", "makemigrations"])
+    subprocess.run(["python", "/home/agl-admin/agl-home-django/agl_home_django/manage.py", "migrate"])
+    subprocess.run(["python", "/home/agl-admin/agl-home-django/agl_home_django/manage.py", "runserver"])
