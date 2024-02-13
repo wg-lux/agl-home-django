cdc3071672669fab07a4ac2ba9ee35fcf5e8af06
maddonix <tlux14@googlemail.com>
2024-02-13 20:22:29 +0100

    asd

--- endoreg_home/endoreg_home/settings_base/static.py
+++ endoreg_home/endoreg_home/settings_base/static.py
@@ -5,7 +5,7 @@ import os
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 STATIC_URL = 'static/'
 
