c59cb02e8de2a01425e9085f80369c7d7423591d
maddonix <tlux14@googlemail.com>
2024-08-11 15:07:57 +0200

    add celery config file

---
+++ endoreg_home/endoreg_home/celery.py
@@ -0,0 +1,19 @@
+# celery.py
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'endoreg_home.settings_prod')
+
+app = Celery('endoreg_home')
+
+# Configure Celery using settings from Django settings.py.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load tasks from all registered Django app configs.
+app.autodiscover_tasks()
+app.conf.broker_connection_retry_on_startup = True
+
