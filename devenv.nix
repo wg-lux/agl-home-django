204d47a61f62e3182762632b1f446959a216b8fe
maxhild <58726316+maxhild@users.noreply.github.com>
2024-12-05 11:11:41 +0100

    devenv.yaml

--- devenv.nix
+++ devenv.nix
@@ -1,104 +1,90 @@
+{ pkgs, lib, config, inputs, ... }:
+let
+  buildInputs = with pkgs; [
+    python312Full
+    # cudaPackages.cuda_cudart
+    # cudaPackages.cudnn
+    stdenv.cc.cc
+  ];
+in
 {
-  description = "Application packaged using uv2nix and managed with devenv";
+  packages = with pkgs; [
+    cudaPackages.cuda_nvcc
+  ];
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
-    devenv.url = "github:cachix/devenv";
-    uv2nix.url = "github:pyproject-nix/uv2nix";
+  env = {
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
   };
 
-  outputs = { self, nixpkgs, flake-utils, devenv, uv2nix, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in {
-        devShells.default = devenv.lib.mkShell {
-          inputs = {
-            inherit pkgs;
-          };
-          modules = [
-            {
-              packages = with pkgs; [
-                libGLU libGL libgcc gcc
-                xorg.libXi xorg.libXmu freeglut
-                xorg.libXext xorg.libX11 xorg.libXv xorg.libXrandr zlib
-                ncurses5 stdenv.cc binutils
-                python311
-                python311Packages.pandas
-                python311Packages.numpy
-              ];
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
 
-              environmentVariables = {
-                DJANGO_SETTINGS_MODULE = "endoreg_home.settings_prod";
-              };
+  scripts = {
+    hello = {
+      exec = "${pkgs.uv}/bin/uv run python hello.py";
+    };
+    run-dev-server = {
+      exec = "${pkgs.uv}/bin/uv run python manage.py runserver";
+    };
+    run-prod-server = {
+      exec = "${pkgs.uv}/bin/uv run daphne devenv_deployment.asgi:application";
+    };
+  };
 
-              shell = {
-                hooks.postShellHook = ''
-                  mkdir -p data
-                  export DJANGO_SETTINGS_MODULE=endoreg_home.settings_prod
-                  export DJANGO_SECRET_KEY=$(cat .env/secret)
-                  export KEYCLOAK_CLIENT=$(cat .env/keycloak-client)
-                  export KEYCLOAK_SECRET=$(cat .env/keycloak-secret)
-                  echo "DJANGO_SECRET_KEY: $DJANGO_SECRET_KEY"
-                  echo "KEYCLOAK_CLIENT: $KEYCLOAK_CLIENT"
-                  echo "KEYCLOAK_SECRET: $KEYCLOAK_SECRET"
-                '';
-              };
+  tasks = {
+    "deploy:make-migrations" = {
+      exec = "${pkgs.uv}/bin/uv run python manage.py makemigrations";
+    };
+    "deploy:migrate" = {
+      exec = "${pkgs.uv}/bin/uv run python manage.py migrate";
+    };
+    "deploy:load-base-db-data" = {
+      exec = "${pkgs.uv}/bin/uv run python manage.py load_base_db_data";
+    };
+    "dev:runserver" = {
+      exec = "${pkgs.uv}/bin/uv run python manage.py runserver";
+    };
+    "prod:runserver" = {
+      exec = "${pkgs.uv}/bin/uv run daphne devenv_deployment.asgi:application";
+    };
+  };
 
-              languages.python = {
-                enable = true;
-                uv = {
-                  enable = true;
-                  sync.enable = true;
-                };
-              };
+  processes = {
+    silly-example = {
+      exec = "while true; do echo hello && sleep 1; done";
+    };
+    ping = {
+      exec = "ping localhost";
+    };
+    nvidia = {
+      exec = "nvidia-smi -l";
+    };
+    django = {
+      exec = "run-prod-server";
+    };
+  };
 
-              scripts = {
-                "run-dev-server" = {
-                  exec = "${pkgs.uv}/bin/uv run python manage.py runserver";
-                };
-                "run-prod-server" = {
-                  exec = "${pkgs.uv}/bin/uv run daphne devenv_deployment.asgi:application";
-                };
-              };
+  hooks = {
+    postShell = ''
+      mkdir -p data
+      export DJANGO_SETTINGS_MODULE=endoreg_home.settings_prod
+      export DJANGO_SECRET_KEY=$(cat .env/secret)
+      export KEYCLOAK_CLIENT=$(cat .env/keycloak-client)
+      export KEYCLOAK_SECRET=$(cat .env/keycloak-secret)
+      echo "DJANGO_SECRET_KEY: $DJANGO_SECRET_KEY"
+      echo "KEYCLOAK_CLIENT: $KEYCLOAK_CLIENT"
+      echo "KEYCLOAK_SECRET: $KEYCLOAK_SECRET"
+    '';
+  };
 
-              tasks = {
-                "deploy:make-migrations" = {
-                  exec = "${pkgs.uv}/bin/uv run python manage.py makemigrations";
-                };
-                "deploy:migrate" = {
-                  exec = "${pkgs.uv}/bin/uv run python manage.py migrate";
-                };
-                "deploy:load-base-db-data" = {
-                  exec = "${pkgs.uv}/bin/uv run python manage.py load_base_db_data";
-                };
-                "dev:runserver" = {
-                  exec = "${pkgs.uv}/bin/uv run python manage.py runserver";
-                };
-                "prod:runserver" = {
-                  exec = "${pkgs.uv}/bin/uv run daphne devenv_deployment.asgi:application";
-                };
-              };
-
-              processes = {
-                django = {
-                  exec = "run-prod-server";
-                };
-              };
-
-              shell = {
-                hooks.enterShell = ''
-                  . .devenv/state/venv/bin/activate
-                  nvcc -V
-                '';
-              };
-            }
-          ];
-        };
-      }
-    );
+  enterShell = ''
+    . .devenv/state/venv/bin/activate
+    nvcc -V
+    hello
+  '';
 }
