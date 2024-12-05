a1832e6acbcdec79cd032f21ed77915eac9ff4e3
maxhild <58726316+maxhild@users.noreply.github.com>
2024-12-05 11:13:47 +0100

    devenv.yaml

--- devenv.nix
+++ devenv.nix
@@ -1,90 +1,56 @@
-{ pkgs, lib, config, inputs, ... }:
-let
-  buildInputs = with pkgs; [
-    python312Full
-    # cudaPackages.cuda_cudart
-    # cudaPackages.cudnn
-    stdenv.cc.cc
-  ];
-in
 {
-  packages = with pkgs; [
-    cudaPackages.cuda_nvcc
-  ];
+  description = "Django development environment using devenv";
 
-  env = {
-    LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
   };
 
-  languages.python = {
-    enable = true;
-    uv = {
-      enable = true;
-      sync.enable = true;
+  outputs = { self, nixpkgs, flake-utils, devenv, ... }: devenv.lib.mkFlake {
+    systems = ["x86_64-linux", "x86_64-darwin"];
+
+    devShells = { system, ... }: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      default = {
+        packages = [
+          pkgs.poetry
+          pkgs.libGLU pkgs.libGL pkgs.libgcc pkgs.gcc
+          pkgs.xorg.libXi pkgs.xorg.libXmu pkgs.freeglut
+          pkgs.xorg.libXext pkgs.xorg.libX11 pkgs.xorg.libXv pkgs.xorg.libXrandr
+          pkgs.zlib pkgs.ncurses5 pkgs.stdenv.cc pkgs.binutils
+          pkgs.python311
+          pkgs.python311Packages.pandas
+          pkgs.python311Packages.numpy
+        ];
+
+        shell = {
+          preShell = ''
+            mkdir -p data
+          '';
+          postShell = ''
+            export DJANGO_SETTINGS_MODULE=endoreg_home.settings_prod
+            export DJANGO_SECRET_KEY=$(cat .env/secret)
+            export KEYCLOAK_CLIENT=$(cat .env/keycloak-client)
+            export KEYCLOAK_SECRET=$(cat .env/keycloak-secret)
+            echo "DJANGO_SECRET_KEY: $DJANGO_SECRET_KEY"
+            echo "KEYCLOAK_CLIENT: $KEYCLOAK_CLIENT"
+            echo "KEYCLOAK_SECRET: $KEYCLOAK_SECRET"
+          '';
+          environmentVariables = {
+            DJANGO_SETTINGS_MODULE = "endoreg_home.settings_prod";
+          };
+        };
+
+        python = {
+          venvDir = ".venv";
+          venvEnabled = true;
+        };
+      };
     };
   };
-
-  scripts = {
-    hello = {
-      exec = "${pkgs.uv}/bin/uv run python hello.py";
-    };
-    run-dev-server = {
-      exec = "${pkgs.uv}/bin/uv run python manage.py runserver";
-    };
-    run-prod-server = {
-      exec = "${pkgs.uv}/bin/uv run daphne devenv_deployment.asgi:application";
-    };
-  };
-
-  tasks = {
-    "deploy:make-migrations" = {
-      exec = "${pkgs.uv}/bin/uv run python manage.py makemigrations";
-    };
-    "deploy:migrate" = {
-      exec = "${pkgs.uv}/bin/uv run python manage.py migrate";
-    };
-    "deploy:load-base-db-data" = {
-      exec = "${pkgs.uv}/bin/uv run python manage.py load_base_db_data";
-    };
-    "dev:runserver" = {
-      exec = "${pkgs.uv}/bin/uv run python manage.py runserver";
-    };
-    "prod:runserver" = {
-      exec = "${pkgs.uv}/bin/uv run daphne devenv_deployment.asgi:application";
-    };
-  };
-
-  processes = {
-    silly-example = {
-      exec = "while true; do echo hello && sleep 1; done";
-    };
-    ping = {
-      exec = "ping localhost";
-    };
-    nvidia = {
-      exec = "nvidia-smi -l";
-    };
-    django = {
-      exec = "run-prod-server";
-    };
-  };
-
-  hooks = {
-    postShell = ''
-      mkdir -p data
-      export DJANGO_SETTINGS_MODULE=endoreg_home.settings_prod
-      export DJANGO_SECRET_KEY=$(cat .env/secret)
-      export KEYCLOAK_CLIENT=$(cat .env/keycloak-client)
-      export KEYCLOAK_SECRET=$(cat .env/keycloak-secret)
-      echo "DJANGO_SECRET_KEY: $DJANGO_SECRET_KEY"
-      echo "KEYCLOAK_CLIENT: $KEYCLOAK_CLIENT"
-      echo "KEYCLOAK_SECRET: $KEYCLOAK_SECRET"
-    '';
-  };
-
-  enterShell = ''
-    . .devenv/state/venv/bin/activate
-    nvcc -V
-    hello
-  '';
 }
